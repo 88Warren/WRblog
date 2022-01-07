e9606007ee035fda7cf837cd892fe7d462c077ac
88warren <warren_laura@yahoo.co.uk>
2022-01-07 16:40:36 +0000

    Backbone

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+const express = require('express');
+const ejsMate = require('ejs-mate');
+const path = require('path');
+
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Serving on port ${PORT}`)
 });
 
+//View engine setup
+app.engine('ejs', ejsMate)
+
+//body Parser replacement Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Additional file directories
+app.set('views', path.join(__dirname, '/views'));
+
 app.get('/', (req, res) => {
-    res.render({title: "Glyn's Gardening Service"})
+    res.render('home.ejs', {title: "Wildroots Kitchen & Bar"})
 })
